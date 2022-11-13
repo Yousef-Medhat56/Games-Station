5fcfd27c935563a3024ab3ce6b45082c82bff24f
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2022-11-13 22:47:00 +0200

    feat: create seed script
    body: the script creates (super admin) record in the admins table

---
+++ prisma/seed.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+const seedSuperAdmin = async () =>
+  await prisma.admin.upsert({
+    where: { email: process.env.SUPER_ADMIN_EMAIL },
+    update: {},
+    create: {
+      email: process.env.SUPER_ADMIN_EMAIL,
+      password: process.env.SUPER_ADMIN_PASSWORD,
+      username: process.env.SUPER_ADMIN_USERNAME,
+      first_name: process.env.SUPER_ADMIN_FIRSTNAME,
+      last_name: process.env.SUPER_ADMIN_LASTNAME,
+      is_super_admin: true,
+    },
+  });
+
+// array for all the seed functions
+const seedFunctionsArr = [seedSuperAdmin];
+
+// loop through the seed functions array and call each function of them
+Promise.all(seedFunctionsArr.map((seedFunc) => seedFunc()))
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
